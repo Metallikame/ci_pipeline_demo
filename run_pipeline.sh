90f9b61c3a8f3037dc8e71e26f3cefae0dfb9540
metal <rex2644@outlook.com>
2025-05-19 17:14:29 +0200

    correction + Ajout du fichier .gitignore

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "---- Début du pipeline CI ----"
-echo "Étape 1 : Lancement des tests..."
+echo "Étape 1 : Lancement des tests avec Python..."
 
-python3 -m unittest test_main.py > output.log 2>&1
+python -m unittest test_main.py > output.log 2>&1
 
 if grep -q "FAILED" output.log; then
     echo "❌ Tests échoués. Interruption du pipeline."
@@ -14,6 +14,17 @@ else
     echo "Étape 2 : Déploiement fictif..."
     echo "[OK] Déploiement effectué."
     echo "$(date) - SUCCÈS" >> logs.txt
+
+    echo "Étape 3 : Vérification du style (PEP8)..."
+    pycodestyle main.py > style_report.txt
+
+    if [ -s style_report.txt ]; then
+        echo "⚠️ Problèmes de style détectés dans main.py :"
+        cat style_report.txt
+    else
+        echo "✅ Style conforme PEP8."
+    fi
 fi
 
 echo "---- Fin du pipeline ----"
+read -p "Appuyez sur Entrée pour fermer..."
